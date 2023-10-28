63ed3d0b5f0be2eceff0e2fef7703ee5344d5203
DanielChombo <danielchombo.dotordaniel@gmail.com>
2023-10-28 23:23:14 +0100

    reate project

---
+++ backend/src/test/java/com/system/softdeliver/SoftdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.softdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
